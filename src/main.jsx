4dc9956ffae0fe20fce0d0e45fd8bb32987db768
TheArrogantWanderer <masroorfazal@gmail.com>
2026-06-28 10:13:32 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AlokaApp from './App.jsx'
+import AlokaApp from '../aloka.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
