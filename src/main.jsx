ab15f62d7bcbc0e2ce0d768df12e8f3083664ddf
TheArrogantWanderer <masroorfazal@gmail.com>
2026-07-02 15:04:32 +0800

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AlokaApp from '../aloka.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AlokaApp />
-  </React.StrictMode>,
-)
